905f665675fb4f34bf25bf4bb56bb0e00837b271
jasdeepsw <130447247+jasdeepsw@users.noreply.github.com>
2023-04-11 16:42:57 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "database-1.cgk3kxzd8dh0.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "jasdeepsinghwalia-employee"
+customregion = "us-east-1"
